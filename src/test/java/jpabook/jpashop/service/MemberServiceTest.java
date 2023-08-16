70360ebf604df954ac6b9522b5b18ae0aa4f6c83
haein-side <kimhi1640@gmail.com>
2023-08-17 05:46:12 +0900

    em.flush()를 통해 쿼리 날리는 방법도 있음

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -16,9 +18,9 @@ class MemberServiceTest {
 
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
     @Test
-    @Rollback(false)
     public void 회원가입 () throws Exception {
         //given
         Member member = new Member();
@@ -26,11 +28,9 @@ class MemberServiceTest {
 
         //when
         Long savedId = memberService.join(member);
-        // persist할 때 쿼리 나가는 게 아니라, commit - flush 되면서 영속성 컨텍스트에 있던 것 반영하는 쿼리 나감
-        // @Rollback(false) 안해두면 rollback 됨
-        // 즉 영속성 컨텍스트를 flush 안 하면서 쿼리문이 안 나가게 되는 것
 
         //then
+        em.flush();
         assertEquals(member, memberRepository.findOne(savedId)); // 같은 영속성 컨텍스트에서 pk값이 같은 엔티티는 딱 하나로만 관리됨
     }
 
