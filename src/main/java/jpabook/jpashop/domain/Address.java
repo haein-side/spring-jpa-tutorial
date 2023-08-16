9374f462996d0cd6269efbb59708ec383e8b4da2
haein-side <kimhi1640@gmail.com>
2023-08-16 16:37:47 +0900

    값 타입은 변경 불가능하게 설계해야

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -5,9 +5,18 @@ import lombok.Getter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter
+@Getter // 값 타입은 변경 불가하게 설계해야
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
