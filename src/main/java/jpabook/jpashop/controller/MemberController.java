df176e78420405c937e052727ba2ad7489e37eee
haein-side <kimhi1640@gmail.com>
2023-08-17 16:44:32 +0900

    화면 간 데이터 전송 시 Entity로 보내거나 받는 게 아니라, DTO로 변환해서 받고 전송할 것

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,17 +29,24 @@ public class MemberController {
     public String create(@Valid MemberForm form, BindingResult result) { //@NotEmpty같은 validation 해줌!, BindingResult 안에 오류 담김
 
         if (result.hasErrors()) {
-            return "members/createMemberForm"; // BindingResult를 통해 어떤 에러가 있는지 화면에 뿌릴 수 있음
+            return "members/createMemberForm"; //BindingResult를 통해 어떤 에러가 있는지 화면에 뿌릴 수 있음
         }
 
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode()); //MemberForm 안에서 만들어도 됨!
 
         Member member = new Member();
-        member.setName(form.getName()); // 중간에 정제
+        member.setName(form.getName()); //중간에 정제
         member.setAddress(address);
 
         memberService.join(member);
         return "redirect:/"; //PRG 패턴 : 저장 후 재로딩되면 안 좋기 때문에 redirect를 많이 씀! - 첫번째 페이지로 감 (멱등성 보장)
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) { //API를 만들 땐 : 반드시 Entity를 DTO로 변환해서 화면에 꼭 필요한 데이터들만 전송할 것 (엔티티 변동 시, API 스팩이 변해버리기 때문)
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -9,6 +9,12 @@ import javax.validation.constraints.NotEmpty;
 public class MemberForm {
     //화면에서 넘어올 때 Validation(memberForm)과 실제 Domain이 원하는 게 다를 수 있으므로 Member 이외에 생성해줌
     //실무에선 input 값 받을 때 복잡함!
+    //엔티티는 화면 종속적인 기능은 Form으로 빼놓자
+    //JPA 쓸 때 조심해야 하는 것 : Entity를 최대한 순수하게 유지해야 함
+    //오직 핵심 비즈니스 로직에만 dependency 있게
+
+    //entity : 실제 테이블과 매핑되는 클래스로 함부로 변경하면 X
+    //DTO : 계층 간 데이터 교환을 위한 객체 - Entity는 그대로 두고 DTO를 화면에 전송할 것
 
     @NotEmpty(message = "회원 이름은 필수입니다.")
     private String name;
