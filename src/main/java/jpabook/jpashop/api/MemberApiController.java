c7244dd384768655f20ef7f875eb8fbd566e58aa
haein-side <kimhi1640@gmail.com>
2023-08-25 16:13:42 +0900

    1-3 회원조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,12 +9,41 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers(); // 엔티티에 있는 정보가 다 노출됨 -> 필드에 @JsonIgnore하면 사라지긴 함 but 다른 api에서 해당 엔티티 사용 시 문제 생김
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDTO> collect = findMembers.stream()
+                .map(m -> new MemberDTO(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect); // data 필드의 값으로 list가 나감 - list를 바로 컬렉션으로 내놓으면 JSON 배열 타입으로 나가기 때문에 유연성이 확 떨어짐
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDTO {
+        private String name; // 딱 노출할 것만 스팩에 노출하는 것
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -23,6 +52,7 @@ public class MemberApiController {
         // 엔티티를 외부에서 바인딩해서 그대로 쓰면 안 됨
         // API 만들 때는 엔티티를 파라미터로 만들면 안 되고 외부에 노출해서도 안 됨
         // 엔티티를 들어가보지 않는 이상, API 문서 까보지 않는 이상 Member에서 어느 값이 파라미터로 넘어오는지 모름
+        // 만약 Member 엔티티에서 username으로 필드명 변경 시, 요청하는 api 스펙에서 JSON을 만들 때 name이 아니라 username으로 모두 변경해줘야 함 (API 스팩이 변동됨)
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
