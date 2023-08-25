a14573f40e18f6912a2d3d1b0d7cde34f3de2981
haein-side <kimhi1640@gmail.com>
2023-08-25 17:23:08 +0900

    3-1 간단한 주문조회 v1:엔티티를 직접 노출 - 엔티티 간 양방향 관계일 때 조회 시 무한 루프

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member (ManyToOne)
+ * Order -> Delivery (OneToOne)
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/sample-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch()); // Order안에 Member가 있고 Member 안에 Order가 있음! -> 양방향 연관관계의 문제점 생감
+        return all;
+    }
+
+}
