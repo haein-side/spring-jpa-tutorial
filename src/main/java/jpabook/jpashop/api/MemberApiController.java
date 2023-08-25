bd66e27830d1ed573039300081b378b2fa05f26c
haein-side <kimhi1640@gmail.com>
2023-08-25 15:19:18 +0900

    1-1 v2: DTO 생성 후2

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +23,7 @@ public class MemberApiController {
         // API 스팩을 위한 별도의 DTO를 만들어야
         // 엔티티를 외부에서 바인딩해서 그대로 쓰면 안 됨
         // API 만들 때는 엔티티를 파라미터로 만들면 안 되고 외부에 노출해서도 안 됨
+        // 엔티티를 들어가보지 않는 이상, API 문서 까보지 않는 이상 Member에서 어느 값이 파라미터로 넘어오는지 모름
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
@@ -29,7 +31,7 @@ public class MemberApiController {
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
-        member.setName(request.getName()); // 엔티티 변경 시 여기서 컴파일 오류가 남 -> 수정 시 api는 영향 안 받음
+        member.setName(request.getName()); // 엔티티 변경 시 여기서 컴파일 오류가 남 -> 수정 시 api는 영향 안 받음 // 엔티티 변경해도 API의 스팩이 변하지 않는다!
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
@@ -37,6 +39,7 @@ public class MemberApiController {
 
     @Data
     static class CreateMemberRequest {
+        @NotEmpty // 각각의 API에서 요구하는 validation들을 DTO에 따로 넣으면 됨! -> Entity에 넣는 것보다 나음 (Entity는 여러 곳에서 사용)
         private String name;
     }
 
