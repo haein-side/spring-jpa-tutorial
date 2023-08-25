00a151c824e96b188545f80d5d2e700b7af958c4
haein-side <kimhi1640@gmail.com>
2023-08-25 15:10:54 +0900

    1-1 v1: DTO 만들기 전 - 문제점들

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -21,6 +21,7 @@ public class MemberApiController {
         // 엔티티와 API의 스택이 1:1 매핑되면 안 됨
         // API 스팩을 위한 별도의 DTO를 만들어야
         // 엔티티를 외부에서 바인딩해서 그대로 쓰면 안 됨
+        // API 만들 때는 엔티티를 파라미터로 만들면 안 되고 외부에 노출해서도 안 됨
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
