9e044c322b54843fd4a113c3101b51316e912e78
haein-side <kimhi1640@gmail.com>
2023-08-25 15:09:56 +0900

    1-1 v1: DTO 만들기 전

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -18,6 +18,9 @@ public class MemberApiController {
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        // 엔티티와 API의 스택이 1:1 매핑되면 안 됨
+        // API 스팩을 위한 별도의 DTO를 만들어야
+        // 엔티티를 외부에서 바인딩해서 그대로 쓰면 안 됨
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
